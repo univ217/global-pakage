f7b69aa200c6674d1e3818ceeb5fc6a9f7bcc9fd
EunJi <univ217@naver.com>
2025-07-01 21:38:40 +0900

    공통 응답 enum 정의 클래스

---
+++ src/main/java/com/hansung/likelion/global/constant/StaticValue.java
@@ -0,0 +1,18 @@
+package com.hansung.likelion.global.constant;
+
+public class StaticValue {
+    // Success
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+
+    // Error
+    public static final int BAD_REQUEST = 400;
+    public static final int UNAUTHORIZED = 401;
+    public static final int FORBIDDEN = 403;
+    public static final int NOT_FOUND = 404;
+    public static final int METHOD_NOT_ALLOWED = 405;
+    public static final int CONFLICT = 409;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+
+    //필요하면 더 추가
+}
